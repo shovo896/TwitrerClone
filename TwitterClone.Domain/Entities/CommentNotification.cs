6c23e1c574568efc71fe0f0b7da8359422218918
Shuvo <suvo10.info@gmail.com>
2026-08-19 20:13:49 +0600

    rhw

--- TwitterClone.Domain/Entities/CommentNotification.cs
+++ TwitterClone.Domain/Entities/CommentNotification.cs
@@ -1,6 +1,16 @@
-﻿namespace TwitterClone.Domain.Entities;
+﻿namespace Twitter_Clone.Entities;
 
-public class CommentNotification
+public class CommentNotification : Notification
 {
-    
+    public CommentNotification(Guid commentByUserId) : base("Comment")
+    {
+        CommentByUserId = commentByUserId;
+    }
+
+    public Guid CommentByUserId { get; set; }
+
+    public void AddMessage(string message)
+    {
+        Message = message;
+    }
 }
