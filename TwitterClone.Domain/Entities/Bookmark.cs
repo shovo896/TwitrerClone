953e12da6f3fa7498ea9ac2f8e4c9f19bd881a89
Shuvo <suvo10.info@gmail.com>
2026-09-01 23:07:12 +0600

    kmopjop

---
+++ TwitterClone.Domain/Entities/Bookmark.cs
@@ -0,0 +1,35 @@
+﻿namespace Twitter_Clone.Entities;
+
+public class Bookmark
+{
+    private Guid _id;
+    private Guid _userId;
+    private Guid _tweetId;
+    private DateTime _createdAt;
+    private DateTime? _modifiedAt;
+    private Guid _createdBy;
+    private Guid? _modifiedBy;
+
+    public Bookmark()
+    {
+        _id = Guid.NewGuid();
+        _createdAt = DateTime.UtcNow;
+    }
+
+    public Guid Id
+    {
+        get { return _id; }
+    }
+
+    public Guid UserId
+    {
+        get { return _userId; }
+        set { _userId = value; }
+    }
+
+    public Guid TweetId => _tweetId;
+    public DateTime CreatedAt => _createdAt;
+    public DateTime? ModifiedAt => _modifiedAt;
+    public Guid CreatedBy => _createdBy;
+    public Guid? ModifiedBy => _modifiedBy;
+}
